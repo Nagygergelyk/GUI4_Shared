ee454c90315ce2b25572f49b61c751cf4c678964
hallgato <hallgato@LABOR215-19>
2022-09-27 09:11:07 +0200

    Logic updated

--- GUI4_Shared/Logic/HeroLogic.cs
+++ GUI4_Shared/Logic/HeroLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI4_Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace GUI4_Shared.Logic
 {
-    public class HeroLogic : ob
+    public class HeroLogic : IHeroLogic
     {
+        public double AVGPower => throw new NotImplementedException();
+
+        public double AVGSpeed => throw new NotImplementedException();
+
+        public void CreateSuperhero(Hero hero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendHome(Hero hero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendToWar(Hero hero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetupCollections(IList<Hero> barracks, IList<Hero> army)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ GUI4_Shared/Logic/IHeroLogic.cs
@@ -0,0 +1,16 @@
+﻿using GUI4_Shared.Models;
+using System.Collections.Generic;
+
+namespace GUI4_Shared.Logic
+{
+    public interface IHeroLogic
+    {
+        double AVGPower { get; }
+        double AVGSpeed { get; }
+
+        void SendToWar(Hero hero);
+        void CreateSuperhero(Hero hero);
+        void SendHome(Hero hero);
+        void SetupCollections(IList<Hero> barracks, IList<Hero> army);
+    }
+}
